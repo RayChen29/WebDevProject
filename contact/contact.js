3d54e4c1b7652db4c5cbd845db11dc04208e8edf
RayChen29 <82558712+RayChen29@users.noreply.github.com>
2023-03-07 13:53:57 -0800

    Made contact form functional.

--- contact/contact.js
+++ contact/contact.js
@@ -1,3 +1,4 @@
-//Credits to EasyTutorials for showing the site https://www.youtube.com/watch?v=sGQSz22U8VM; Email Code from https://smtpjs.com/
-
-let url = "https://smtpjs.com/v3/smtp.js";
+let button = document.getElementsByClassName("btn")[0];
+button.addEventListener("click", (event) => {
+  button.style.display = "none";
+});
