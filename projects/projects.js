f42b6316ab9f425b733b0e7ae8d873b113e8d256
RayChen29 <82558712+RayChen29@users.noreply.github.com>
2023-03-07 13:32:32 -0800

    Fixed buttons to appear cleaner and prettier

--- projects/projects.js
+++ projects/projects.js
@@ -5,13 +5,15 @@ const project3 = "https://github.com/RayChen29/TrafficGraphProjectcs";
 const violet = "#aa66dd";
 const gray = "#a9b3b3";
 const blue = "#7c90c9";
+const white = "#ffffff";
+const black = "#000000";
 
-let link1 = document.getElementsByClassName("project-link1")[0];
+let link1 = document.getElementsByClassName("project1")[0];
 link1.addEventListener("mouseover", (event) => {
   link1.style.backgroundColor = violet;
 });
 link1.addEventListener("mouseout", (event) => {
-  link1.style.backgroundColor = gray;
+  link1.style.backgroundColor = white;
 });
 link1.addEventListener("mousedown", (event) => {
   link1.style.backgroundColor = blue;
@@ -23,12 +25,12 @@ link1.addEventListener("mouseup", (event) => {
   link1.style.backgroundColor = violet;
 });
 
-let link2 = document.getElementsByClassName("project-link2")[0];
+let link2 = document.getElementsByClassName("project2")[0];
 link2.addEventListener("mouseover", (event) => {
   link2.style.backgroundColor = violet;
 });
 link2.addEventListener("mouseout", (event) => {
-  link2.style.backgroundColor = gray;
+  link2.style.backgroundColor = white;
 });
 link2.addEventListener("mousedown", (event) => {
   link2.style.backgroundColor = blue;
@@ -40,12 +42,12 @@ link1.addEventListener("mouseup", (event) => {
   link2.style.backgroundColor = violet;
 });
 
-let link3 = document.getElementsByClassName("project-link3")[0];
+let link3 = document.getElementsByClassName("project3")[0];
 link3.addEventListener("mouseover", (event) => {
   link3.style.backgroundColor = violet;
 });
 link3.addEventListener("mouseout", (event) => {
-  link3.style.backgroundColor = gray;
+  link3.style.backgroundColor = white;
 });
 link3.addEventListener("mousedown", (event) => {
   link3.style.backgroundColor = blue;
