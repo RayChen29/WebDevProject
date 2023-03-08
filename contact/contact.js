886c8f394ac8648fa6846cbf142b624bfa65e4d7
RayChen29 <82558712+RayChen29@users.noreply.github.com>
2023-03-07 17:20:54 -0800

    Fields are required before sending.

--- contact/contact.js
+++ contact/contact.js
@@ -2,3 +2,31 @@ let button = document.getElementsByClassName("btn")[0];
 button.addEventListener("click", (event) => {
   button.style.display = "none";
 });
+
+const form = document.getElementById("contact-form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const messageInput = document.getElementById("message");
+const submitButton = document.querySelector(".btn");
+
+const validateForm = () => {
+  const nameValue = nameInput.value.trim();
+  const emailValue = emailInput.value.trim();
+  const messageValue = messageInput.value.trim();
+
+  if (
+    nameValue === "" ||
+    emailValue === "" ||
+    messageValue === "" ||
+    emailValue.indexOf("@") === -1
+  ) {
+    submitButton.disabled = true;
+  } else {
+    submitButton.disabled = false;
+    submitButton.backgroundColor = "#FFFFFF";
+  }
+};
+
+nameInput.addEventListener("input", validateForm);
+emailInput.addEventListener("input", validateForm);
+messageInput.addEventListener("input", validateForm);
